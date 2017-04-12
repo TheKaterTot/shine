9bc54724528d58950885f7e7f5901b43bf1066bf
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-11 20:48:10 -0600

    move camera with player

--- client/app.js
+++ client/app.js
@@ -4,21 +4,45 @@ const Player = require('./player');
 const Npc = require('./npc');
 const Bump = require('bump.js');
 
-let renderer = new PIXI.Application(800, 600, { backgroundColor: '0xD3D3D3' });
-let container = new PIXI.Container();
-let bump = new Bump(PIXI);
+class Game {
+  constructor() {
+    this.app = new PIXI.Application(800, 600, { backgroundColor: '0xD3D3D3' });
+    this.container = new PIXI.Container();
+    this.bump = new Bump(PIXI);
+    this.player = new Player(25, 175);
+    this.npc = new Npc(400, 300);
+    this.app.stage.addChild(this.container);
+    this.container.addChild(this.player);
+    this.container.addChild(this.npc);
+    this.map = { width: 2000, height: 2000 };
 
-renderer.stage.addChild(container);
+    this.app.ticker.add((deltaTime) => {
+      this.player.tick(deltaTime);
+      this.bump.hit(this.player, this.npc, true);
+    });
 
-let player = new Player(25, 175);
-let npc = new Npc(400, 300);
+    document.querySelector('#game-screen').appendChild(this.app.view);
 
-container.addChild(player);
-container.addChild(npc);
+    this.player.on('change', this.moveCamera.bind(this));
+  }
 
-renderer.ticker.add(function(deltaTime) {
-  player.tick(deltaTime);
-  bump.hit(player, npc, true);
-});
+  moveCamera(player, dx, dy) {
+    let changeX = 0;
+    let changeY = 0;
+    let halfWidth = this.app.renderer.width / 2.0;
+    let halfHeight = this.app.renderer.height / 2.0;
 
-document.querySelector('#game-screen').appendChild(renderer.view);
+    if (player.x > halfWidth && player.x < this.map.width - halfWidth) {
+      changeX = dx;
+    }
+    if (player.y > halfHeight && player.y < this.map.height - halfHeight) {
+      changeY = dy;
+    }
+
+    this.app.stage.pivot.x += changeX;
+    this.app.stage.pivot.y += changeY;
+  }
+
+}
+
+new Game();
--- client/player.js
+++ client/player.js
@@ -1,5 +1,5 @@
-const PIXI = require('pixi.js')
-const ArcadeKeys = require('arcade_keys')
+const PIXI = require('pixi.js');
+const ArcadeKeys = require('arcade_keys');
 
 let arcadeKeys = ArcadeKeys();
 
@@ -13,18 +13,26 @@ class Player extends PIXI.Sprite {
   }
 
   tick(deltaTime) {
+    let dy = 0;
+    let dx = 0;
+
     if (arcadeKeys.isPressed(ArcadeKeys.keys.up)) {
       this.y -= this.speed * deltaTime;
+      dy -= this.speed * deltaTime;
     }
     else if (arcadeKeys.isPressed(ArcadeKeys.keys.down)) {
       this.y += this.speed * deltaTime;
+      dy += this.speed * deltaTime;
     }
     else if (arcadeKeys.isPressed(ArcadeKeys.keys.left)) {
       this.x -= this.speed * deltaTime;
+      dx -= this.speed * deltaTime;
     }
     else if (arcadeKeys.isPressed(ArcadeKeys.keys.right)) {
       this.x += this.speed * deltaTime;
+      dx += this.speed * deltaTime;
     }
+    this.emit('change', this, dx, dy);
   }
 }
 
