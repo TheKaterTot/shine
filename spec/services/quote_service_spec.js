2439c8131dd91f13fd39ee033b5f1bc467a258a4
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-15 11:34:40 -0600

    Set up quote service

---
+++ spec/services/quote_service_spec.js
@@ -0,0 +1,17 @@
+const nock = require('nock');
+const expect = require('expect');
+const quoteService = require('../../services/quote_service')
+
+describe('quote service', function() {
+  it('returns quote objects', function(done) {
+    nock('https://favqs.com')
+      .get('/api/quotes/')
+      .query({filter: 'dreadpiratekaty', type: 'user'})
+      .reply(200, {quotes: [1, 2, 3, 4]});
+
+    quoteService(function(error, quotes) {
+      expect(quotes).toEqual([1, 2, 3, 4]);
+      done();
+    });
+  })
+})
