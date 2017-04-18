159718309b4935518ebcd8ef3eafa315b03292c8
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-17 19:43:28 -0600

    Add heroku prep

--- models/base.js
+++ models/base.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const databaseUrl = {
   test: 'mongodb://localhost/shine_test',
   development: 'mongodb://localhost/shine_development',
-  production: process.env.DATABASE_URL
+  production: process.env.MONGODB_URI
 }[process.env.NODE_ENV || 'development']
 
 let client = mongoose.connect(databaseUrl);
