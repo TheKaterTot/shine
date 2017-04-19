0042861f1b33383b62134a4c6492f58a3abfa421
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-18 21:13:41 -0600

    Add player shine

--- client/player.js
+++ client/player.js
@@ -4,15 +4,57 @@ const generateID = require('./entity');
 
 let arcadeKeys = ArcadeKeys();
 
-class Player extends PIXI.Sprite {
+class PlayerSprite extends PIXI.Sprite {
 
-  constructor(x, y) {
+  constructor() {
     super(PIXI.Texture.fromImage('/images/player_0.png'));
+    this.shiny = 0;
+  }
+
+  getShiny() {
+    if (this.shiny === 4) {
+      return;
+    }
+
+    this.shiny++;
+    this.texture = PIXI.Texture.fromImage(`/images/player_${this.shiny}.png`);
+  }
+}
+
+class PlayerGraphic extends PIXI.Graphics {
+  constructor() {
+    super();
+    this.shiny = 0.0;
+    this.draw();
+  }
+
+  draw() {
+    this.beginFill(0x19B5FE, this.shiny);
+    this.drawRect(-2, -2, 34, 34);
+    this.endFill();
+  }
+
+  getShiny() {
+    if (this.shiny === 1.0) {
+      return;
+    }
+    this.shiny += 0.25;
+    this.clear();
+    this.draw();
+  }
+}
+
+class Player extends PIXI.Sprite {
+  constructor(x, y) {
+    super(PIXI.Texture.fromImage('/images/background.png'));
+    this.sprite = new PlayerSprite();
+    this.graphic = new PlayerGraphic();
+    this.addChild(this.graphic);
+    this.addChild(this.sprite);
+    this.speed = 1.5;
     this.x = x;
     this.y = y;
-    this.speed = 1.5;
     this.id = generateID();
-    this.shiny = 0;
   }
 
   tick(deltaTime) {
@@ -39,13 +81,8 @@ class Player extends PIXI.Sprite {
   }
 
   getShiny() {
-    if (this.shiny === 4) {
-      return;
-    }
-
-    this.shiny++;
-    this.texture = PIXI.Texture.fromImage(`/images/player_${this.shiny}.png`);
+    this.sprite.getShiny();
+    this.graphic.getShiny();
   }
 }
-
 module.exports = Player;
